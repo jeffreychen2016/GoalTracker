2e5619aad1e156e520dc6150b7e436422315e4ea
Jeffrey Chen <jeffreychen2016@gmail.com>
2019-01-28 17:30:35 -0600

    added login form

--- GoalTracker/ClientApp/src/components/Login/Login.js
+++ GoalTracker/ClientApp/src/components/Login/Login.js
@@ -1,10 +1,90 @@
 import React, { Component } from 'react';
+import {FormGroup,InputGroup,FormControl} from 'react-bootstrap';
 
 export class Login extends Component {
+    
+  state = {
+    user: {
+      email: 'jeffreychen2016@gmail.com',
+      password: '123456',
+    },
+  };
+
+  emailChange = e => {
+    const tempUser = { ...this.state.user };
+    tempUser.email = e.target.value;
+    this.setState({ user: tempUser });
+  };
+
+  passwordChange = e => {
+    const tempUser = { ...this.state.user };
+    tempUser.password = e.target.value;
+    this.setState({ user: tempUser });
+  };
+
+  // loginClickEvent = (e) => {
+  //   const { user } = this.state;
+  //   e.preventDefault();
+  //   authRequests
+  //     .loginUser(user)
+  //     .then(() => {
+  //       this.props.history.push('/counter');
+  //     })
+  //     .catch(error => {
+  //       console.error('there was an error while trying to login', error);
+  //     });
+  // };
+
   render() {
+    const { user } = this.state;
     return (
-      <div>
-        <h1>Login</h1>
+      <div className="Login">
+        <div id="login-form">
+          <h1 className="text-center">Login</h1>
+          <form className="form-horizontal col-sm-6 col-sm-offset-3">
+            <div className="form-group">
+              <div className="input-group">
+                <span className="input-group-addon" id="basic-addon1">
+                  <span className="glyphicon glyphicon-user"></span>
+                </span>
+                <input 
+                  type="text" 
+                  className="form-control" 
+                  placeholder="Email" 
+                  aria-describedby="basic-addon1" 
+                  value={user.email}
+                  onChange={this.emailChange}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="input-group">
+                <span className="input-group-addon" id="basic-addon1">
+                  <span className="glyphicon glyphicon-lock"></span>
+                </span>
+                <input 
+                  type="password" 
+                  className="form-control" 
+                  placeholder="Password" 
+                  aria-describedby="basic-addon1" 
+                  value={user.password}
+                  onChange={this.passwordChange}
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <div className="col-sm-12">
+                <button
+                  type="submit"
+                  className="btn btn-default col-xs-12"
+                  onClick={this.loginClickEvent}
+                >
+                  Login
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
