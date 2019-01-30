8844c3f1f52e90316befa2927caf3575fe28be09
Jeffrey Chen <jeffreychen2016@gmail.com>
2019-01-29 19:41:57 -0600

    created goalpanel component

---
+++ GoalTracker/ClientApp/src/components/GoalPanel/GoalPanel.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class GoalPanel extends Component {
+  displayName = GoalPanel.name
+
+  render() {
+    return (
+      <div>
+        <h1 className="text-center">Goal Panel</h1>
+      </div>
+    );
+  }
+}
--- GoalTracker/ClientApp/src/components/Profile/Profile.js
+++ GoalTracker/ClientApp/src/components/Profile/Profile.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { GoalPanel } from '../GoalPanel/GoalPanel';
 
 export class Profile extends Component {
   displayName = Profile.name
@@ -7,6 +8,7 @@ export class Profile extends Component {
     return (
       <div>
         <h1 className="text-center">User Profile</h1>
+        <GoalPanel />
       </div>
     );
   }
