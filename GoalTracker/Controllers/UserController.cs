ec8398a2166f6e7c74b128c77aa74021fc0ad61c
Jeffrey Chen <jeffreychen2016@gmail.com>
2019-01-29 18:22:54 -0600

    created api end point for adding new users

---
+++ GoalTracker/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GoalTracker.DataAccess;
+using GoalTracker.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace GoalTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserAccess _userAccess;
+
+        public UserController(IConfiguration config)
+        {
+            _userAccess = new UserAccess(config);
+        }
+
+        [HttpPost("adduser")]
+        public IActionResult AddUser(User user)
+        {
+            return Ok(_userAccess.AddUser(user));
+        }
+    }
+}
